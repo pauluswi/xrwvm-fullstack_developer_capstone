edaf18aec3e6f9ffe843eb4de1f129effaac6739
pauluswi <35646814+pauluswi@users.noreply.github.com>
2024-12-07 22:25:53 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,9 +7,13 @@ from django.conf import settings
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout_user, name='logout'),
 
     # path for dealer reviews view
 
